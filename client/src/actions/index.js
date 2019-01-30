620616f815bca9c02850b5b10c8c5629861f4379
jwpaine <jpaine@protonmail.com>
2019-01-29 19:50:42 -0500

    redux-forms, redux-thunk

---
+++ client/src/actions/index.js
@@ -0,0 +1,8 @@
+import { AUTH_USER } from './types';
+
+export const signup = ({email, password}) => {
+	return {
+		type: auth_user,
+		payload: '123213123'
+	};
+};
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class Signup extends Component {
+
+	onSubmit = formProps => {
+		console.log(formProps)
+	}
+
 	render() {
+		const { handleSubmit } = this.props;
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit)}>
 				<fieldset>
 					<label>Email</label>
+					<Field
+						name="email"
+						type="text"
+						component="input"
+						autoComplete="none"
+					/>
 				</fieldset>
 				<fieldset>
 					<label>Password</label>
+					<Field
+						name="password"
+						type="password"
+						component="input"
+						autoComplete="none"
+					/>
 				</fieldset>
+
+				<button>Sign Up!</button>
 
 			</form>
 		)
 	}
 }
 
-export default Signup;
+export default reduxForm({ form: 'signup' })(Signup)
